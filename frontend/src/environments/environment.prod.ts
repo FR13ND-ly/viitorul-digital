6653a6226fb915e04089909aaaf65e1146c1018b
FR13ND-ly <57838113+FR13ND-ly@users.noreply.github.com>
2023-06-17 15:07:57 +0300

    Update environment.prod.ts

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBXFfMFXmLAzvX_Skxm3EILYY2nfEOIL84",
+    authDomain: "viitorul-digital.firebaseapp.com",
+    projectId: "viitorul-digital",
+    storageBucket: "viitorul-digital.appspot.com",
+    messagingSenderId: "67170119935",
+    appId: "1:67170119935:web:c7c1f84d1a11c7acd25cca"
+  },
+  apiUrl: "https://viitoruldigital.pythonanywhere.com/api/",
 };
